1ebdf53c04ba0b2103cfa1dbfae0b11bc1a6feb1
Batbold N <bxtbold@protonmail.com>
2024-01-23 23:07:56 +0900

    fix: update test csv file directory

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use cli::Cli;
 use utils::*;
 
 fn main() {
-    let path = "/home/bat/ws/rust_ws/todo/lists/20240121.csv";
+    let path = "/history/20240121.csv";
     let cli = Cli::new();
     cli.read_today(path);
 }
