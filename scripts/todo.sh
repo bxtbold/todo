1672f7e76880bc147fb2af7a707cb19c8d1f380b
bxtbold <bxtbold@protonmail.com>
2024-02-04 22:09:29 +0900

    chore: merge bash scripts

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,25 +1,39 @@
-todo() {
+#!/bin/bash
 
-    echo ""
-    case "$1" in
-        add)
-            echo "Adding $2"
-            ;;
-        done)
-            echo "Completed $2"
-            ;;
-        rm)
-            echo "Removing $2"
-            ;;
-        list)
-            echo "Listing"
-            ;;
-        sort)
-            echo "Sorting"
-            ;;
-        *)
-            echo "Unknown command: $1"
-            exit 1
-            ;;
-    esac
+todo() {
+    script_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    cd $script_path/..
+
+    local binary_debug_path="$(pwd)/target/debug/todo"
+    local binary_release_path="$(pwd)/target/release/todo"
+
+    if [ -x "$binary_debug_path" ]; then
+        "$binary_debug_path" "$@"
+    elif [ -x "$binary_release_path" ]; then
+        "$binary_release_path" "$@"
+    else
+        echo "Error: Binary not found. Building..."
+        cargo build --release
+        "$binary_release_path" "$@"
+    fi
 }
+
+_todo_completion() {
+    if [ "$2" == "add" ]; then
+        COMPREPLY=( )
+    elif [ "$2" == "done" ]; then
+        COMPREPLY=( )
+    elif [ "$2" == "gui" ]; then
+        COMPREPLY=( )
+    elif [ "$2" == "rm" ]; then
+        COMPREPLY=( )
+    elif [ "$2" == "list" ]; then
+        COMPREPLY=( )
+    elif [ "$2" == "sort" ]; then
+        COMPREPLY=( )
+    else
+        COMPREPLY=( $(compgen -W "add done gui rm list sort" -- "$2") )
+    fi
+}
+
+complete -F _todo_completion todo
